bf0c573507f45eebe9e50be1d4c24ea8c7740e66
EnriqueFlores <72516617+EnriqueFloresAq@users.noreply.github.com>
2021-01-25 16:44:48 -0500

    Cambio realizado para comprobar los commit
    Se añadió un cometario

--- src/redux/action/tokenAction.js
+++ src/redux/action/tokenAction.js
@@ -39,7 +39,7 @@ export const insertToken = () => dispatch => {
     })
   }
 }
-
+//Comentario para ver las cosas
 export const POST_TOKEN_BEGIN = 'POST_TOKEN_BEGIN'
 export const POST_TOKEN_SUCCESS = 'POST_TOKEN_SUCCESS'
 export const POST_TOKEN_FAIL = 'POST_TOKEN_FAIL'
